e97cbac139e356cadd9c287db733e7df390bde00
praveendiwakar <praveen.diwakar1@gmail.com>
2026-09-04 17:31:37 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+base = int(input('Enter base number:'))
+exponent = int(input('Enter the exponent:'))
+
+result = 1
+
+for i in range (1, exponent + 1 ):
+    result = result * base
+    print("step", i , ": result =")
